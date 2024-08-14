c779b7e5e76a8aaaa85dccd309813d26a83550bc
aamna-ansari <aamnansari29@gmail.com>
2024-08-14 10:36:29 +0500

    lisklist

--- LinkedLists/Linklist_insert & remove.py
+++ LinkedLists/Linklist_insert & remove.py
@@ -81,6 +81,19 @@ class LinkedList:
         temp.next = new_node
         self.length += 1   
         return True  
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return None
+        if index == 0:
+            return self.pop_first()
+        if index == self.length - 1:
+            return self.pop()
+        pre = self.get(index - 1)
+        temp = pre.next
+        pre.next = temp.next
+        temp.next = None
+        self.length -= 1
+        return temp
 
 l = LinkedList(5)
 l.prepend(3)
