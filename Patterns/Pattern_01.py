3f078a924f8cf526541373b58c3775e75957c0b0
aamna-ansari <aamnansari29@gmail.com>
2025-03-16 19:39:47 +0500

    Add triangle pattern printing to Pattern_01.py

--- Patterns/Pattern_01.py
+++ Patterns/Pattern_01.py
@@ -7,3 +7,14 @@ for i in range(4):  # Outer loop for rows
     for j in range(4):  # Inner loop for columns
         print("*", end=" ")  # Print '*' without a newline
     print()  # Move to the next line after each row
+
+
+#   * 
+#   * *  
+#   * * * 
+#   * * *
+
+for i in range(4):
+    for j in range(i+1):
+        print("*", end=" ")
+    print()
