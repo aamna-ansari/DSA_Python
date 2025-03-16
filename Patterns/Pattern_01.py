08fdc5f548b9f9c832b18978894f2f0dab360189
aamna-ansari <aamnansari29@gmail.com>
2025-03-16 19:36:42 +0500

    Add pattern printing example in Pattern_01.py

---
+++ Patterns/Pattern_01.py
@@ -0,0 +1,9 @@
+#   * * * * 
+#   * * * * 
+#   * * * * 
+#   * * * * 
+
+for i in range(4):  # Outer loop for rows
+    for j in range(4):  # Inner loop for columns
+        print("*", end=" ")  # Print '*' without a newline
+    print()  # Move to the next line after each row
