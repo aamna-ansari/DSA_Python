58c28a4fecc3b61406bb614b0fe4fb258c35d29d
aamna-ansari <aamnansari29@gmail.com>
2025-03-18 14:21:19 +0500

    Add additional number patterns to Pattern_01.py

--- Patterns/Pattern_01.py
+++ Patterns/Pattern_01.py
@@ -18,17 +18,23 @@ for i in range(4):
     for j in range(i+1):
         print("*", end=" ")
     print()
-
+# Pattern 3
 for i in range(4):  # Controls the rows
     for j in range(i + 1):  # Controls the numbers in each row
         print(j + 1, end=" ")  # Print numbers starting from 1
     print()  # Move to the next line
 
+
+
+# Pattern 4
 for i in range(4):  # Controls the rows
     for j in range(i+1):  # Controls the numbers in each row
         print(i+1, end=" ")  # Print numbers starting from 1
     print()
 
+
+    
+# Pattern 5
     for i in range(4):  # Controls the rows
         for j in range(4 - i):  # Controls the numbers in each row
             print(j+1, end=" ")  # Print numbers starting from 1
