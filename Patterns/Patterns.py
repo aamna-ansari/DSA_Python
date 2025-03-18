f902a4cfab5180f0795631a4ea30d6da26b69cf0
aamna-ansari <aamnansari29@gmail.com>
2025-03-18 19:41:58 +0500

    # Pattern 7

--- Patterns/Patterns.py
+++ Patterns/Patterns.py
@@ -39,3 +39,17 @@ for i in range(4):  # Controls the rows
         for j in range(4 - i):  # Controls the numbers in each row
             print(j+1, end=" ")  # Print numbers starting from 1
         print()
+
+
+# Pattern 7
+for i in range(6): # outer loop 
+    # space inner 
+    for j in range(6-i): # inner loop
+        print(" ", end=" ")
+    # Star 
+    for j in range(2* i+1):
+        print("*", end=" ")
+    # Space
+    for j in range(6-i):
+        print(" ", end=" ")
+    print()
